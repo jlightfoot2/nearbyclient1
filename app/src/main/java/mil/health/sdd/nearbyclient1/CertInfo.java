d9c9c14f39bad17e1585280525e95412c84316da
Jack Lightfoot <jack.lightfoot@tee2.org>
2018-09-14 15:35:10 -0700

    some fragment files

---
+++ app/src/main/java/mil/health/sdd/nearbyclient1/CertInfo.java
@@ -0,0 +1,54 @@
+package mil.health.sdd.nearbyclient1;
+
+public class CertInfo {
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getCn() {
+        return cn;
+    }
+
+    public void setCn(String cn) {
+        this.cn = cn;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String toString(){
+        return this.getCn() + ", " + this.getOrganization() + ", " + this.getCountry() + ", " + this.getState() + ", " + this.getLocality();
+    }
+
+    private String organization;
+    private String cn;
+    private String state;
+    private String locality;
+    private String country;
+
+}
---
+++ app/src/main/java/mil/health/sdd/nearbyclient1/fragments/X509CertFragment.java
@@ -0,0 +1,88 @@
+package mil.health.sdd.nearbyclient1.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import mil.health.sdd.nearbyclient1.CertInfo;
+import mil.health.sdd.nearbyclient1.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class X509CertFragment extends Fragment {
+    CaCertificateListener mCallback;
+    CertInfo certInfo;
+
+    public X509CertFragment() {
+
+    }
+@Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (CaCertificateListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_x509_cert, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Set values for view here
+        TextView tTitle = (TextView) view.findViewById(R.id.textViewFragmentTitle);
+        TextView tCountry = (TextView) view.findViewById(R.id.textViewFragmentCountry);
+        TextView tState = (TextView) view.findViewById(R.id.textViewFragmentState);
+        TextView tLocality = (TextView) view.findViewById(R.id.textViewFragmentLocality);
+        TextView tOrganization = (TextView) view.findViewById(R.id.textViewFragmentOrganization);
+
+
+
+        Button bDelete = (Button) view.findViewById(R.id.buttonFragmentDelete);
+
+        // update view
+
+        if(certInfo == null){
+            tTitle.setText("Empty Cert");
+        } else {
+            tTitle.setText(certInfo.getCn());
+            tCountry.setText(certInfo.getCountry());
+            tState.setText(certInfo.getState());
+            tLocality.setText(certInfo.getLocality());
+            tOrganization.setText(certInfo.getOrganization());
+        }
+        bDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               mCallback.onClickDelete();
+            }
+        });
+    }
+
+    public void setCert(CertInfo certInfo){
+        this.certInfo = certInfo;
+    }
+
+    public interface CaCertificateListener {
+        public void onClickDelete();
+    }
+
+}
